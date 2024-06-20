f6aeb0cc94680b48d633853fbf64a800f5433ea3
stenio bomfim <stenio_lima2016@hotmail.com>
2024-06-20 16:11:18 -0300

    Initial Commit

---
+++ chatappwithFirebase/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  chatappwithFirebase
+//
+//  Created by stenio Lima on 20/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ chatappwithFirebase/chatappwithFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatappwithFirebaseApp.swift
+//  chatappwithFirebase
+//
+//  Created by stenio Lima on 20/06/24.
+//
+
+import SwiftUI
+
+@main
+struct chatappwithFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
